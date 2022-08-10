9bca4cdb101a0c7370aae46321ad1cf55ccad6dc
Freddie Brewin <freddiebrewin@live.com>
2022-08-10 22:13:37 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UndrawImage from '../assets/undraw_movie.svg'
 
 const Home = () => {
     return (
@@ -8,6 +9,13 @@ const Home = () => {
                     <div className='header__description'>
                         <h1>The UK's #1 Movie Streaming Service</h1>
                         <h2>Find all your favorite <b className='red'>Movies</b> using <b className='red'>MovieAPI</b></h2>
+                        <figure>
+                            <img src={UndrawImage} alt="" className='homescreen__logo'/>
+                            <div className='input__wrapper'>
+                                <input type="text" name="" id="" placeholder='Search for your favorite film here' className='search__input'/>
+                            </div>
+                        </figure>
+                        
                     </div>
                 </div>
             </header>
