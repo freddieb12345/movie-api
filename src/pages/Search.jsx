c08580487fe7273584696f31ee42062e012255eb
Freddie Brewin <freddiebrewin@live.com>
2022-08-11 23:12:47 +0100

    commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import Movie from './ui/movie';
 
 const Search = () => {
     const apiKey = "ac8e032ce18cd4b026afad6cd3298ad8"
+    const filmSearch = "fast & furious"
     
     async function getMovies() {
-        const response = await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`)
-        console.log(response)
+        const { data } = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${filmSearch}`)
+        console.log(data)
     }
 
     useEffect(() => {
@@ -14,8 +16,29 @@ const Search = () => {
     }, [])
     
     return (
-        <div>
-            Hello
+        <div id='movies__body'>
+            <main id='movies__main'>
+                <section>
+                    <div className="movies__container">
+                        <div className="row">
+                            <div className="movies__header">
+                                <h2 className='section__title'>
+                                    All Books
+                                </h2>
+                                <select id="filter" defaultValue="DEFAULT">
+                                    <option value="DEFAULT" disabled>Sort</option>
+                                    <option value="LOW_TO_HIGH">Ascending price</option>
+                                    <option value="HIGH_TO_LOW">Descending price</option>
+                                    <option value="RATING">Rating</option>
+                                </select>
+                            </div>
+                            <div className='movies'>
+                                {new Array(10).fill(0).map((_, index) => ( <Movie/>))}
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 }
