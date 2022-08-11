9e703ddf40920379005d8be444178cf685fa4814
Freddie Brewin <freddiebrewin@live.com>
2022-08-11 17:57:22 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UndrawImage from '../assets/undraw_movie.svg'
 
 const Home = () => {
+    const [userSearch, setUserSearch] = useState()
+
+    function search() {
+        console.log(userSearch)
+    }
     return (
-        <section>
+        <section id='home'>
             <header>
                 <div className='header__container'>
                     <div className='header__description'>
-                        <h1>The UK's #1 Movie Streaming Service</h1>
+                        <h1 className='homepage__header'>The UK's #1 Movie Streaming Service</h1>
                         <h2>Find all your favorite <b className='red'>Movies</b> using <b className='red'>MovieAPI</b></h2>
-                        <figure>
-                            <img src={UndrawImage} alt="" className='homescreen__logo'/>
-                            <div className='input__wrapper'>
-                                <input type="text" name="" id="" placeholder='Search for your favorite film here' className='search__input'/>
-                            </div>
-                        </figure>
-                        
                     </div>
+                    <div className='input__wrapper'>
+                        <input 
+                            type="text"
+                            name="" 
+                            id="" 
+                            placeholder='Search for your favorite film here...' 
+                            className='search__input' 
+                            onChange={(event) =>setUserSearch(event.target.value)}/>
+                        <button className='search__button' onClick={() => search()}>Search</button>
+                    </div>
+                    <figure>
+                            <img src={UndrawImage} alt="" className='homescreen__logo'/>
+                    </figure>
                 </div>
             </header>
         </section>
