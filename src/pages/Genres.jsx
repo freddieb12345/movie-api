35c4ac3db325b8761dab6bb1f64956c75b3279ad
Freddie Brewin <freddiebrewin@live.com>
2022-08-14 21:17:08 +0100

    commit

--- src/pages/Genres.jsx
+++ src/pages/Genres.jsx
@@ -1,44 +1,163 @@
-import React, {useState, useEffect} from 'react';
-import Slider from '../components/NetflixSlider'
-import axios from 'axios'
+import React, {useEffect} from 'react';
+// import axios from 'axios'
+import "../styles/slider.css"
 
 const Genres = () => {
-    const apiKey = "ac8e032ce18cd4b026afad6cd3298ad8";
-    const [genres, setGenres] = useState([]);
+    // const apiKey = "ac8e032ce18cd4b026afad6cd3298ad8";
+    // const [genresData, setGenresData] = useState([]);
     
-    async function getMovieGenres() {
-        const {data: {genres}} = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
-        console.log(genres)
-        setGenres(genres)
-    }
+    // useEffect(() => {
+    //     async function getMovieGenres() {
+    //     const {data: {genres}} = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
+    //     setGenresData(genres.map((genre) => (genre.id)))
+    // }
+    // }, [])
 
-    async function getMoviesByGenre() {
-        const {data: {results}} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=35`)
-        console.log(results)
-    }
+    // useEffect(() => {
+    //     console.log(genresData)
+    // }, [genresData])
+    
+    //What this does is it checks that if the current thing that was clicked was the handle, it makes that the handle variable, otherwise, if we clicked within the
+    //handle, then the parent of the thing we clicked on is the handle. Once the handle is not equal to null, it runs the handleClick function
+    document.addEventListener("click", e => {
+        let handle
+        if(e.target.matches(".handle")) {
+            handle = e.target
+        } else {
+            handle = e.target.closest(".handle")
+        }
+        if(handle != null) onHandleClick(handle)
+    })
+
+    const throttledProgressBar = throttle(() => {
+        document.querySelectorAll(".progress__bar").forEach(calculateProgressBar)
+    }, 250) 
+
+    window.addEventListener("resize", throttledProgressBar)
     
     useEffect(() => {
-        getMovieGenres()
-    }, [])
+        document.querySelectorAll(".progress__bar").forEach(calculateProgressBar)
+    },[])
 
-    useEffect(() => {
-        console.log(genres)
-    }, [genres])
+    function calculateProgressBar(progressBar) {
+        progressBar.innerHTML = ""
+        const slider = progressBar.closest(".slider__row").querySelector(".slider")
+        const itemCount = slider.children.length
+        const itemsPerScreen = parseInt(getComputedStyle(slider).getPropertyValue("--items-per-screen"))
+        let sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+        const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen)
 
-    // getMovieGenres()
-    // getMoviesByGenre()
+        if(sliderIndex >= progressBarItemCount) {
+            slider.style.setProperty("--slider-index", progressBarItemCount - 1)
+            sliderIndex = progressBarItemCount - 1
+        }
+        
+        for(let i = 0 ; i < progressBarItemCount; i++) {
+            const barItem = document.createElement("div")
+            barItem.classList.add("progress__item")
+            if(i === sliderIndex) {
+                barItem.classList.add("active")
+            }
+            progressBar.append(barItem)
+        }
+    }
+
+    function onHandleClick(handle) {
+        //The slider const gets the slider that is within the container that is closest to the handle that was clicked 
+        const progressBar = handle.closest(".slider__row").querySelector(".progress__bar")
+        const slider = handle.closest(".slider__container").querySelector(".slider") 
+        
+        const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+        const progressBarItemCount = progressBar.children.length
+        
+        if(handle.classList.contains("left-handle")) {
+            if(sliderIndex - 1 < 0) {
+                slider.style.setProperty("--slider-index", progressBarItemCount - 1)
+                progressBar.children[sliderIndex].classList.remove("active")
+                progressBar.children[progressBarItemCount - 1].classList.add("active")
+            } else {
+                slider.style.setProperty("--slider-index", sliderIndex - 1)
+                progressBar.children[sliderIndex].classList.remove("active")
+                progressBar.children[sliderIndex - 1].classList.add("active")
+            }
+        }
+
+        if(handle.classList.contains("right-handle")) {
+            if(sliderIndex + 1 >= progressBarItemCount) {
+                slider.style.setProperty("--slider-index", 0)
+                progressBar.children[sliderIndex].classList.remove("active")
+                progressBar.children[0].classList.add("active")
+            } else {
+                slider.style.setProperty("--slider-index", sliderIndex + 1)
+                progressBar.children[sliderIndex].classList.remove("active")
+                progressBar.children[sliderIndex + 1].classList.add("active")
+            }
+        }
+    }
+
+    function throttle(cb, delay = 1000) {
+        let shouldWait = false
+        let waitingArgs
+        const timeoutFunc = () => {
+            if(waitingArgs == null) {
+                shouldWait = false
+            } else {
+                cb(...waitingArgs)
+                waitingArgs = null
+                setTimeout(timeoutFunc, delay)
+            }
+        }
+        return (...args) => {
+            if (shouldWait) {
+                waitingArgs = args
+                return
+            }
+
+            cb(...args)
+            shouldWait = true
+            setTimeout(timeoutFunc, delay)
+        }
+    }
+
     return (
-        <div className="app">
-        <Slider>
-            <Slider.Item movie="movie1" key="1">item1</Slider.Item>
-            <Slider.Item movie="movie1" key="2">item1</Slider.Item>
-            <Slider.Item movie="movie1" key="3">item1</Slider.Item>
-            <Slider.Item movie="movie1" key="4">item1</Slider.Item>
-            <Slider.Item movie="movie1" key="5">item1</Slider.Item>
-            <Slider.Item movie="movie1" key="6">item1</Slider.Item>
-            <Slider.Item movie="movie1" key="7">item1</Slider.Item>
-        </Slider>
-      </div>
+        <div className='slider__row'>
+            <div className='slider__header'>
+                <h3 className='slider__title'>Title</h3>
+                <div className="progress__bar">
+                    <div className="progress__item active"></div>
+                    <div className="progress__item"></div>
+                    <div className="progress__item"></div>
+                    <div className="progress__item"></div>
+                    <div className="progress__item"></div>
+                </div>
+            </div>
+            <div className="slider__container">
+                <button className="handle left-handle">
+                    <div className="text">
+                        &#8249;
+                    </div>
+                </button>
+                <div className='slider'>
+                    <img src="https://via.placeholder.com/210/00FF00?text=1" alt="" />
+                    <img src="https://via.placeholder.com/220/00FF00?text=2" alt="" />
+                    <img src="https://via.placeholder.com/230/00FF00?text=3" alt="" />
+                    <img src="https://via.placeholder.com/240/00FF00?text=4" alt="" />
+                    <img src="https://via.placeholder.com/250/00FF00?text=5" alt="" />
+                    <img src="https://via.placeholder.com/260/00FF00?text=6" alt="" />
+                    <img src="https://via.placeholder.com/270/00FF00?text=7" alt="" />
+                    <img src="https://via.placeholder.com/280/00FF00?text=8" alt="" />
+                    <img src="https://via.placeholder.com/290/00FF00?text=9" alt="" />
+                    <img src="https://via.placeholder.com/300/00FF00?text=10" alt="" />
+                    <img src="https://via.placeholder.com/310/00FF00?text=11" alt="" />
+                    <img src="https://via.placeholder.com/320/00FF00?text=12" alt="" />
+                </div>
+                <button className="handle right-handle">
+                    <div className="text">
+                        &#8250;
+                    </div>
+                </button>
+            </div>
+        </div>
     );
 }
 
